f5ef828fc8fde4918b703c397c9ddf897f92228a
Wasim Akram <mr.saim516@gmail.com>
2024-12-28 21:09:31 +0500

    enable cors

--- server.js
+++ server.js
@@ -7,7 +7,15 @@ require("dotenv").config();
 const app = express();
 
 // Middleware
-app.use(cors());
+app.use(cors({
+  origin: [
+    "http://localhost:3000", // Frontend local development
+     // Frontend production URL
+  ],
+  credentials: true, // Enable if you are using cookies for authentication
+  methods: ["GET", "POST", "PUT", "DELETE"], // Allowed HTTP methods
+  allowedHeaders: ["Content-Type", "Authorization"], // Allowed headers
+}));
 app.use(bodyParser.json());
 
 // Routes
@@ -34,6 +42,6 @@ app.use(errorHandler);
 // Start Server
 const PORT = process.env.PORT || "5000";
 const BaseUrl = process.env.BASE_URL || "http://localhost";
- app.listen(PORT, () => {
-    console.log(`Server running on ${BaseUrl}:${PORT}`);
-  });
+app.listen(PORT, () => {
+  console.log(`Server running on ${BaseUrl}:${PORT}`);
+});
